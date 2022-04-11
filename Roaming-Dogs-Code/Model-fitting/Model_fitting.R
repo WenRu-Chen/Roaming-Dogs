931b9bc93c11eecd809747ba6c3b2daab8f2b30f
WenRu-Chen <spi890012@gmail.com>
2022-04-11 13:52:40 +0800

    add GWPCA reference

--- Roaming-Dogs-Code/Model-fitting/Model_fitting.R
+++ Roaming-Dogs-Code/Model-fitting/Model_fitting.R
@@ -15,46 +15,10 @@ library(GGally)       # 相關係數圖矩陣（scatter plot matrix）
 library(maptools)
 library(MASS)
 library(tmap)
-getwd()
 
-# Load data ####
-path = 'Roaming-Dogs-Data\\'
-df <- read.csv("Roaming-Dogs-Data/@Test_KS/Variable.csv", fileEncoding = 'utf-8')
-Variable_KS<-shapefile("Roaming-Dogs-Data/@Test_KS/Variable.shp",warnPRJ = F)
-area(Variable_KS)
-# Xy ####
-col_X = c("Market","Cluster",   
-           "high_rat",   "mid_rat",    "low_rat",   
-          "Hospital" ,  "Clinic", "M_F_RAT" ,    
-           "P_DEN",      "DEPENDENCY", "YOUN_DEP",   "OLD_DEP",    "AGING_IDX",     
-           "Ele" ,       "Ele_stu",    "Junior" ,   
-           "Junior_stu", "Senior",     "Senior_stu", "TaxPayer" ,  "Income_tot", "Income_mea",
-           "Income_med" ,"Income_Q1",  "Income_Q3",  "Income_sta", "Income_CV",  "Temple",    
-           "Train.stat", "Train.crow" )
-col_y = c('Nt')
+# Function ####
 
-col_X_count =c("Market","Hospital" ,  "Clinic",    
-              "Ele" ,"Ele_stu","Junior" ,"Junior_stu", "Senior","Senior_stu", 
-              "TaxPayer" ,  "Income_tot",  "Temple","Train.stat", "Train.crow" )
-
-for (i in c(col_y,col_X_count)) {
-  df[i] = df[i]/(df['Area']*10000)
-  
-}
-
-# cor ####
-
-corr = cor(df[c(col_y,col_X)])
-col_income = c( "Income_mea","Income_med" ,"Income_Q1", 
-                "Income_Q3",  "Income_sta", "Income_CV")
-corr_income = sort(corr[col_income,'Nt'])
-idx = abs(corr[,'Nt'])>.15
-col_X_02 = names(corr[idx,'Nt']) %>% tail(-1)
-
-
-# pdf plot ####
-
-
+# * Plot ####
 pdf_plot <- function(x){
   g = ggplot()+
     geom_histogram(aes(x = x, y = ..density..), 
@@ -66,86 +30,72 @@ pdf_plot <- function(x){
   return(g)
 }
 
-Nt_pdf = pdf_plot(df$Nt)+xlab('Nt')
+# Load data ####
+getwd()
+path = 'Roaming-Dogs-Data\\'
+Variable_KS_df <- read.csv(paste0(path, "@Test_KS\\Variable.csv" ), fileEncoding = 'utf-8')
+Variable_KS_df["Clinic"][is.na(Variable_KS_df["Clinic"])] = 0
+Variable_KS_shp<-shapefile(paste0(path, "@Test_KS\\Variable.shp" ),encoding = 'big5')
+crs(Variable_KS_shp) <- CRS('+init=EPSG:3826') 
 
-# Poisson regression ####
+# *轉數值 ####
+for(i in c(col_X, col_y)){
+  
+  Variable_KS_shp@data[i] = sapply(Variable_KS_df[i], function(x) as.numeric(x))
+}
 
-Fit_Po <-glm(Nt~.,data=df[c(col_y,col_X_02)],family=poisson()) #建poisson 
-summary(Fit_Po) #查看回归模型参数
-pdf_plot(Fit_Po$residuals)+xlab('residuals')
+# Select X y ####
 
-# VIF
-# https://www.statology.org/variance-inflation-factor-r/
+col_X = 
+  c( "Cluster", # 分群
+     "Market","Hospital" ,  "Temple",  "Ele" ,"Junior" ,"Senior", "Train.stat",  "Clinic", # 公共建設
+     "high_rat",   "mid_rat","low_rat", "M_F_RAT" , "P_DEN", "YOUN_DEP","OLD_DEP","AGING_IDX", # 人口統計(教育程度、人口密度...)
+     "Income_mea", "Income_med" ,"Income_Q1",  "Income_Q3",  "Income_sta") # 村里收入
+col_y = c('Nt')
 
-idx = (vif(Fit_Po)<10); col_X_03 = names(vif(Fit_Po)[idx])
+# *轉換成密度 (間/平方公里)
 
-Fit_Po_02 <-glm(Nt~.,data=df[c(col_y,col_X_03)],family=poisson()) #建poisson
+for(i in c('Market','Hospital',  "Temple",  "Ele" ,"Junior" ,"Senior", "Train.stat",  "Clinic")){
+  
+  i_new = paste0(i, "_den")
+  print(i_new)
+  
+  Variable_KS_shp@data[i_new] = Variable_KS_shp@data[i]/Variable_KS_shp@data$Area_sqkm
+  Variable_KS_df[i_new] = Variable_KS_df[i]/Variable_KS_df$Area_sqkm
+}
 
-summary(Fit_Po_02) #查看回归模型参数
-pdf_plot(Fit_Po_02$residuals)+xlab('residuals')
+# *收入range ####
+Variable_KS_shp@data["Income_Q3-Q1"] = Variable_KS_shp@data["Income_Q3"]-Variable_KS_shp@data["Income_Q1"]
+Variable_KS_df["Income_Q3-Q1"] = Variable_KS_df["Income_Q3"]-Variable_KS_df["Income_Q1"]
+
+# *new X ####
+col_X = 
+  c( "Cluster", # 分群
+     "Market_den","Hospital_den" ,  "Temple_den",  "Ele_den" ,"Junior_den" ,"Senior_den", "Train.stat_den",  "Clinic_den", # 公共建設
+     "high_rat",   "mid_rat","low_rat", "M_F_RAT" , "P_DEN", "YOUN_DEP","OLD_DEP","AGING_IDX", # 人口統計(教育程度、人口密度...)
+     "Income_mea","Income_sta")
+
+# cor ####
+
+corr = cor(Variable_KS_df[c(col_y,col_X)])
+col_income = c( "Income_mea","Income_med" ,"Income_Q3-Q1",  "Income_sta", "Income_CV")
+corr_income = cor(Variable_KS_df[c(col_y,col_income)])
+idx = abs(corr[,'Nt'])>.15
+col_X_02 = names(corr[idx,'Nt']) %>% tail(-1)
 
 
-# NB ####
-# https://blog.csdn.net/weixin_54000907/article/details/117915956
-# 定義 theta 
+# GWPCA 處理共縣性問題 ####
+
+DM<-gw.dist(dp.locat=data.matrix(((Variable_KS_df[c('X', "Y")]))))
 
 
-Fit_NB = glm.nb(Nt~.,data=df[c(col_y,col_X_02)], link = log)
+bw.gwpca.basic <-  # 25
+  bw.gwpca(Variable_KS_shp, vars = col_X_02, k =3, robust = FALSE, adaptive = TRUE, dMat = DM)
 
-summary(Fit_NB)
-pdf_plot(Fit_NB$residuals)+xlab('residuals')
-
-# VIF
-# https://www.statology.org/variance-inflation-factor-r/
-
-idx = (vif(Fit_NB)<10); col_X_03 = names(vif(Fit_NB)[idx])
-
-Fit_NB_02 = glm.nb(Nt~.,data=df[c(col_y,col_X_03)], link = log)
-summary(Fit_NB_02) #查看回归模型参数
-pdf_plot(Fit_NB_02$residuals)+xlab('residuals')
+gwpca.basic <- gwpca(Variable_KS_shp,
+                     vars = col_X_02, bw = bw.gwpca.basic, k = 3, robust = FALSE, adaptive = TRUE)
 
 
+# bw.gwpca.robust <-
+# bw.gwpca(Variable_KS_shp, vars = col_X_02, k = 3, robust = TRUE, adaptive = TRUE, dMat = DM )
 
-#---
-
-theta = logtrans(Nt~.,data=df[c(col_y,col_X_02)])
-theta = theta$x[theta$y == max(theta$y)]
-
-Fit_NB_2 = glm(Nt~.,data=df[c(col_y,col_X_03)], family = negative.binomial(theta))
-idx = (vif(Fit_NB_2)<10); col_X_03 = names(vif(Fit_NB_2)[idx])
-summary(Fit_NB_2)
-pdf_plot(Fit_NB_2$residuals)+xlab('residuals')
-
-# GRPW ####
-# https://zia207.github.io/geospatial-r-github.io/geographically-weighted-poisson-regression.html
-
-Variable_KS<-shapefile("Roaming-Dogs-Data/@Test_KS/Variable.shp",warnPRJ = F)
-class(Variable_KS)
-
-formula.GWPR = Nt~.
-DM<-gw.dist(dp.locat=coordinates(Variable_KS))
-
-bw <- bw.ggwr(formula.GWPR,  
-              data = Variable_KS[,c(col_X_03, col_y)],
-              family = "poisson",
-              approach = "AICc",
-              kernel = "gaussian", 
-              adaptive = TRUE,
-              dMat = DM )
-
-GWPR_model <- ggwr.basic(formula.GWPR, 
-                         data =Variable_KS[,c(col_X_03, col_y)],
-                         family = "poisson",
-                         bw = bw, 
-                         kernel = "gaussian", 
-                         adaptive = TRUE,
-                         dMat = DM)
-summary(GWPR_model)
-pdf_plot(GWPR_model$glms$residuals)+xlab('residuals')
-
-## 探索性因子分析 ####
-# https://zhuanlan.zhihu.com/p/37440281
-library(psych)
-corr = cor(df[c(col_y,col_X)])
-fa.parallel(corr, n.obs = 58, fa = "both", n.iter = 100, main = "平行分析碎石图")
-fa <- fa(corr, nfactors = 5, rotate = "none", fm = "pa")
